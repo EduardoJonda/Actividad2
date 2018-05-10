5ec77705b2e509d899dae5a382333be223f0a8fa
Mac Tecsup <>
2018-05-09 19:47:24 -0500

    Complete

--- Actividad2/ViewController.swift
+++ Actividad2/ViewController.swift
@@ -29,8 +29,6 @@ class ViewController: UIViewController {
             
             switch(fromSegment.selectedSegmentIndex){
             case 0:
-                
-                
                 resultado = millasConverter(inicial: numero, toSegment: toSegment.selectedSegmentIndex)
                 
                 finalValue.text = "\(fixData(valor: numero)) \(getType(index: fromSegment.selectedSegmentIndex)) = \(fixData(valor: resultado)) \(getType(index: toSegment.selectedSegmentIndex))"
@@ -171,15 +169,12 @@ class ViewController: UIViewController {
         label.text = "\(fixData(valor: initialValue)) \(getType(index: fromValue)) = \(fixData(valor: finalValue)) \(getType(index: toValue))"
     }
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
